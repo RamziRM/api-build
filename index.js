5b1bb549ad6002bb66c2e5751bafa726af1d0b8a
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-11 02:32:23 -0300

    testing

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const PORT = 8080;
 
-app.listen(PORT, () =>
-  console.log(`Server is running on port http://localhost:${PORT}`)
-);
+app.use(express.json());
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "👕",
+    size: "large",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+app.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+  if (!logo) {
+    res.status(418).send({ message: "We need a logo!" });
+  }
+
+  res.send({
+    tshirt: `👕 with your ${logo} and ID of ${id}`,
+  });
+});
